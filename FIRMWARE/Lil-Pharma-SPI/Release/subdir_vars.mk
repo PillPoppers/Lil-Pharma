bea88a2d4969c0498ff9cd80e8615aa9f1c76aef
power-max <maximpoe@gmail.com>
2018-10-29 23:25:58 -0400

    pushing initial release (does not compile!) to github.

---
+++ FIRMWARE/Lil-Pharma-SPI/Release/subdir_vars.mk
@@ -0,0 +1,73 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../main.cpp 
+
+CMD_SRCS += \
+../cc3200v1p32.cmd 
+
+C_SRCS += \
+../gfxfont.c \
+../glcdfont.c \
+../pinMux.c \
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/startup_ccs.c \
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/timer_if.c \
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/uart_if.c 
+
+C_DEPS += \
+./gfxfont.d \
+./glcdfont.d \
+./pinMux.d \
+./startup_ccs.d \
+./timer_if.d \
+./uart_if.d 
+
+OBJS += \
+./gfxfont.obj \
+./glcdfont.obj \
+./main.obj \
+./pinMux.obj \
+./startup_ccs.obj \
+./timer_if.obj \
+./uart_if.obj 
+
+CPP_DEPS += \
+./main.d 
+
+OBJS__QUOTED += \
+"gfxfont.obj" \
+"glcdfont.obj" \
+"main.obj" \
+"pinMux.obj" \
+"startup_ccs.obj" \
+"timer_if.obj" \
+"uart_if.obj" 
+
+C_DEPS__QUOTED += \
+"gfxfont.d" \
+"glcdfont.d" \
+"pinMux.d" \
+"startup_ccs.d" \
+"timer_if.d" \
+"uart_if.d" 
+
+CPP_DEPS__QUOTED += \
+"main.d" 
+
+C_SRCS__QUOTED += \
+"../gfxfont.c" \
+"../glcdfont.c" \
+"../pinMux.c" \
+"C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/startup_ccs.c" \
+"C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/timer_if.c" \
+"C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/uart_if.c" 
+
+CPP_SRCS__QUOTED += \
+"../main.cpp" 
+
+
---
+++ FIRMWARE/Lil-Pharma-SPI/arduino.h
@@ -0,0 +1,69 @@
+/*
+ * arduino.h
+ *
+ *  Created on: Oct 26, 2018
+ *      Author: maxim
+ */
+
+// Standard includes
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+
+// Driverlib includes
+#include "hw_types.h"
+#include "hw_memmap.h"
+#include "hw_common_reg.h"
+#include "hw_ints.h"
+#include "spi.h"
+#include "rom.h"
+#include "rom_map.h"
+#include "utils.h"
+#include "prcm.h"
+#include "uart.h"
+#include "interrupt.h"
+#include "timer.h"
+
+#ifndef ARDUINO_H_
+#define ARDUINO_H_
+
+// define some basic functions:
+#define min(a,b) ((a)>(b)?(b):(a))
+#define max(a,b) ((a)>(b)?(a):(b))
+#define delay(i) MAP_UtilsDelay(80*100*i)
+
+volatile uint32_t millis = 0;
+
+
+// use arduino macro if you prefer:
+#define OUTPUT GPIO_DIR_MODE_OUT
+#define INPUT  GPIO_DIR_MODE_IN
+#define HIGH 1
+#define LOW  0
+
+// note: The CC3200 is not an AVR ATmegaxxx MCU. So the following functions are similar to arduino, but not exactly the same. (maybe different arguments.)
+
+
+// digitalWrite, similar to arduino, will take a pin number and will write it HIGH or LOW.
+// it just abstracts the GPIOPinWrite function a bit in a arduino library-friendly way.
+
+// the GPIO pin is specified based on the labeling given on the datasheet. (GPIO 0 -- 18, 22 --
+// (it seems that GPIOPinWrite is really just a thin abstraction of raw port manipulation.
+//void digitalWrite(uint16_t pin, bool state){
+//
+//    GPIOPinWrite()
+//}
+
+
+//*****************************************************************************
+//
+//! The interrupt is used to implement millis() and micros() and can be modified
+//! to allow software PWM to be implemented on any pin. for the first timer interrupt.
+//!
+//! \param  None
+//! \return none
+//
+//*****************************************************************************
+
+
+#endif /* ARDUINO_H_ */
